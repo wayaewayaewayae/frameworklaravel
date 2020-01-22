321862cdb98fddcaeba4d160b661fd6678d7fc2a
wayaewayaewayae <santiatia2003@gmail.com>
2020-01-22 11:55:32 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,82 @@ Route::get ('Biodata/{nama}/{Alamat}/{Tgllahir} ', function ($a,$b,$c)
         .'<br>Alamat : '.$b
         .'<br>tgl lahir : '.$c;
 });
+
+
+//Route Opisional Parameter
+Route::get ('Pesan/{a?}/{b?}{hrg}', function ($mkn=null,$nmn=null,$hrg=null){
+    if (isset($mkn)){
+        $mkn = "Anda Memesan $mkn";
+        echo $mkn;
+    }
+
+    if (isset($nmn)){
+        $nmn = "& $nmn";
+        echo $nmn;
+    }
+    if (!$mkn && !$nmn){
+        return 'Anda Belum Memesan Sesuatu';
+    }
+        if(isset($hrg)){
+            $Size =" Dengan Harga $ " .$hrg;
+            echo "$hrg";
+
+            if($hrg<=0){
+                echo "Harga Tidak Valid";
+            }
+            elseif ($hrg >= 25000) {
+                echo "dan size medium";
+            }
+            elseif ($hrg <25000) {
+                echo "dan size small";
+            }
+        }
+});
+
+//Route Opisional Parameter Umur
+Route::get ('tni/{nama?}/{beratbadan?}/{umur?} ', function ($a=null,$b=null,$c=null){
+
+    if (isset($a)){
+        $a = "Nama Anda : $a<br>";
+        echo $a;
+    }
+
+    if (isset($b)){
+        $h = "Berat Badan : $b<br>";
+        echo $h;
+    }
+
+     if (isset($c)){
+        $v = "Umur : $c tahun<br>";
+        echo $v;
+    }
+    if(isset($b)){
+
+            if ($b >= 76) {
+                echo "Anda Harus Menurunkan Berat Badan";
+            }
+            else if ($b >= 100) {
+                echo "Anda Harus Menurunkan Berat Badan";
+            }
+            elseif ($b < 50 ) {
+                echo "Anda Kurang Nutrisi";
+            }
+        }
+        if (!$c && !$b){
+             if ($c = 30 >= 40) {
+                echo "Perwira";
+            }
+            else if ($c = 40 >= 50) {
+                echo "Laksamana";
+            }
+            elseif ($c = 60 ) {
+                echo "Jendral";
+        }
+    }
+});
+
+
+
+
+
+
